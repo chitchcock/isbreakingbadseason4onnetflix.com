2189a1cb6b1e1fbfb1fb0fe8f49e856031714e9f
Chris Hitchcock <christopher.hitchcock@gmail.com>
2014-03-13 19:05:37 -0700

    Initial commit of files.

---
+++ README.md
@@ -0,0 +1,4 @@
+Install vitural env
+virtualenv --no-site-packages bb_netflix_env
+pip install requests==0.13.0
+pip install python-netflix
---
+++ run_is_bb_s4_on_netflix.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+source ~/bb_netflix_env/bin/activate
+cd ~/projects/bb_netflix/is_seasonx_on_netflix
+python is_seasonx_on_netflix.py
